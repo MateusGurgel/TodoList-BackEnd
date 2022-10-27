cc1046f0bfcab4119bb3db82728414e688900183
Mateus Gurgel <mateusgurgel15@gmail.com>
2022-10-27 13:18:56 -0300

    Password and Username Max and min length Changed

--- app/Validators/CreateUserValidator.ts
+++ app/Validators/CreateUserValidator.ts
@@ -5,8 +5,8 @@ export default class CreateUserValidator {
   constructor(protected ctx: HttpContextContract) {}
 
   public schema = schema.create({
-    username: schema.string([rules.maxLength(30)]),
-    password: schema.string([rules.minLength(6), rules.maxLength(16)]),
+    username: schema.string([rules.minLength(6) , rules.maxLength(30)]),
+    password: schema.string([rules.minLength(8), rules.maxLength(32)]),
     email: schema.string([
       rules.email(),
       rules.normalizeEmail({
