16f42a1f2ebf03db87fa9736d2578e07f152babb
Mateus Gurgel <mateusgurgel15@gmail.com>
2022-10-29 02:11:21 -0300

    CreateTaskValidator Created

---
+++ app/Validators/CreateTaskValidator.ts
@@ -0,0 +1,44 @@
+import { schema, CustomMessages, rules } from '@ioc:Adonis/Core/Validator'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class CreateTaskValidator {
+  constructor(protected ctx: HttpContextContract) {}
+
+  /*
+   * Define schema to validate the "shape", "type", "formatting" and "integrity" of data.
+   *
+   * For example:
+   * 1. The username must be of data type string. But then also, it should
+   *    not contain special characters or numbers.
+   *    ```
+   *     schema.string({}, [ rules.alpha() ])
+   *    ```
+   *
+   * 2. The email must be of data type string, formatted as a valid
+   *    email. But also, not used by any other user.
+   *    ```
+   *     schema.string({}, [
+   *       rules.email(),
+   *       rules.unique({ table: 'users', column: 'email' }),
+   *     ])
+   *    ```
+   */
+  public schema = schema.create({
+    title: schema.string([rules.minLength(1), rules.maxLength(50)]),
+    description: schema.string([rules.minLength(1), rules.maxLength(1000)]),
+    priority: schema.enumSet([0, 1, 2]),
+    done: schema.boolean.optional(),
+  })
+  /**
+   * Custom messages for validation failures. You can make use of dot notation `(.)`
+   * for targeting nested fields and array expressions `(*)` for targeting all
+   * children of an array. For example:
+   *
+   * {
+   *   'profile.username.required': 'Username is required',
+   *   'scores.*.number': 'Define scores as valid numbers'
+   * }
+   *
+   */
+  public messages: CustomMessages = {}
+}
